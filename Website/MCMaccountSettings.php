03d953b260dd54b68663aae50dff7cc658f4615e
kaoyama <kd268@nau.edu>
2012-12-04 17:02:59 -0700

    Added lists of subscribed merchants for ad and coupon sections. Updated the feasibility document.

--- Website/MCMaccountSettings.php
+++ Website/MCMaccountSettings.php
@@ -36,7 +36,7 @@
         #adSection
         {
             background: #FFFFFF;
-            width: 20%; 
+            width: 37%; 
             float: left; 
             margin: 0px; 
             padding: 10px;
@@ -44,13 +44,98 @@
         #couponSection
         {
             background: #FFFFFF;
-            width: 20%; 
+            width: 38%; 
+            float: left; 
+            margin: 0px; 
+            padding: 10px;
+        }
+        #main {
+            overflow:auto;
+            padding-bottom: 100px;
+        }  /* must be same height as the footer */
+        #footer {
+            position: relative;
+            margin-top: -100px; /* negative value of footer height */
+            height: 100px;
+            clear:both;
+        } 
+        #merchantList
+        {
+            background: forestgreen;
+            border-width: 3px;
+            border-style: solid;
+            border-color: #000000;
+            width: 30%; 
             float: left; 
             margin: 0px; 
             padding: 10px;
         }
     </style>
+<?php
+session_start();
+$user = "kimi";//$_SESSION['user'];
+$dbase = "test";
+$host = "localhost";
+    $cxn = mysql_connect($host) or die ("No connection possible");
+    $dbr = mysql_select_db($dbase,$cxn)or die(mysql_error());
 
+   if ($dbr == FALSE) 
+       echo "<h6>DB Error: ".mysql_error($cxn)."</h6>";
+   //get merchants that this user is subscribed to
+   $getAdMerchants = "SELECT * FROM subscribedForAds WHERE userName = '$user'";
+   $adMerchants = mysql_query($getAdMerchants, $cxn);
+   $adMerchantList = "";
+   $noAdMerchant="";
+   //$merchants
+   //if there are none, no one has ads
+   if (mysql_num_rows($adMerchants) < 1){
+       //you are not subscribed to any merchants for ads
+       $noAdMerchant = "You are not subscribed to get any ads from any merchants.";
+   }
+   else {
+       //loop through to get array of merchants
+       $adRow = mysql_fetch_row($adMerchants);
+       $i=0;
+       for ($i=1;$i < sizeof($adRow);$i++)
+       {
+           if ($adRow[$i] != "" && $adRow[$i] != null)
+           {
+           $adMerchantList = $adMerchantList.$adRow[$i]."#";
+           }
+       }
+              // echo "<h2>$adMerchantList</h2>";
+       
+   }
+   $stringthing = "hi";
+   $adMerchantList = $adMerchantList.$stringthing;
+   $getCouponMerchants = "SELECT * FROM subscribedForCoupons WHERE userName = '$user'";
+   $couponMerchants = mysql_query($getCouponMerchants, $cxn);
+   $couponMerchantList = "";
+   if (mysql_num_rows($couponMerchants) < 1)
+   {
+       //you are not subscribed to any merchants for coupons
+       $noCouponMerchant = "You are not subscribed to get any coupons from any merchants.";
+   }
+   else
+   {
+       //loop through to get array of merchants
+       $couponRow = mysql_fetch_row($couponMerchants);
+       $i=0;
+       for ($i=1;$i < sizeof($couponRow);$i++)
+       {
+           if ($couponRow[$i] != "" && $couponRow[$i] != null)
+           {
+               $couponMerchantList = $couponMerchantList.$couponRow[$i]."#";
+               $blah = sizeof($couponRow[$i]);
+              // echo "row: $i   size  $blah";
+           }
+           
+       }
+      // echo "<h2>$couponMerchantList</h2>";
+   }
+  
+//put them in an array
+ ?>  
     <body>
 
         <div class="navbar navbar-fixed-top">
@@ -78,24 +163,32 @@
 
             <h2>Edit Account Settings</h2>
             
+            <div id="leftSidebar">
+                <h3>Account Options</h3>
             <label for="locatlization">Localization?</label>
             <input type = 'checkbox' name = 'localization'/>
             
             <label for="ads">Ads?</label>
             <input type='radio' name='ads' id='noAds' value='none' onclick="return noAdsDisplay()"/>No Ads <br />
-            <input type='radio' name='ads' id='someAds' value='some' onclick="return selectAds()"/>Select Ads <br />
+            <input type='radio' name='ads' id='someAds' value='some' onclick=
+                <?php 
+                echo "'return selectAds(\"$adMerchantList\")'/>Select Ads <br />"
+                ?>
             <input type='radio' name='ads' id='allAds' value='all' checked="checked" onclick="return allAds()" />All Ads
-            
             <label for="coupons">Coupons?</label>
             <input type='radio' name='coupons' id='noCoupons' value='none' onclick="return noCouponsDisplay()"/>No Coupons <br />
-            <input type='radio' name='coupons' id='someCoupons' value='some' onclick="return selectCoupons()"/>Select Coupons <br />
+            <input type='radio' name='coupons' id='someCoupons' value='some' onclick=
+            <?php
+            echo "'return selectCoupons(\"$couponMerchantList\")'/>Select Coupons <br />"
+            ?>
             <input type='radio' name='coupons' id='allCoupons' value='all' checked="checked" onclick="return allCoupons()" />All Coupons
-
-            <div id="stuff"></div>
+            </div>
+            <div id="adSection"><h3>Advertisement Options</h3></div>
+            <div id="couponSection"><h3>Coupon Options</h3></div>
             <hr>
-            <footer>
-                <p>MoneyClip Mobile 2012</p>
-            </footer>
+            <div id="footer">
+                <footer><p>MoneyClip Mobile 2012</p></footer>
+            </div>
         </div>
     </body>
 </html>
--- Website/settingsOptions.js
+++ Website/settingsOptions.js
@@ -3,20 +3,67 @@
  * and open the template in the editor.
  */
 function noAdsDisplay() {
-    document.getElementById("stuff").innerHTML = "<label> No ads homie </label>";
+    document.getElementById("adSection").innerHTML = "<h3>Advertisement Options</h3>\n\
+<label> You are not receiving any ads. </label>";
 }
-function selectAds() {
-    document.getElementById("stuff").innerHTML = "<label>  some ads homie </label>";
+
+function selectAds(merchantList) {
+    var listOfMerchants = new Array();
+    listOfMerchants = merchantList.split("#");
+    var htmlString = "<h3>Advertisement Options</h3>\n\
+<label for='adSortType'>Sort ads by:</label>\n\
+<select name=sortType id=sortType>\n\
+<option value='productType'>Product Type</option>\n\
+<option value='alphaNum'>Alphabetical</option>\n\
+<option value='distance'>Distance</option>\n\
+</select>\n\
+<br/>\n\
+<div id='merchantList'>";
+    
+    for (var i=0;i<listOfMerchants.length;i++)
+        {
+            htmlString += "<label for='" + listOfMerchants[i] + "'>" + listOfMerchants[i] + "</label>";
+            htmlString += "<input type='checkbox' name='" + listOfMerchants[i] + "' />";
+        }
+           htmlString += "</div>";
+
+   document.getElementById("adSection").innerHTML = htmlString;
+   
 }
 function allAds() {
-    document.getElementById("stuff").innerHTML = "<label> all ads homie </label>";
+    document.getElementById("adSection").innerHTML = "<h3>Advertisement Options</h3>\n\
+<label> You are receiving ads from the following merchants.</label>";
 }
 function noCouponsDisplay() {
-    document.getElementById("stuff").innerHTML = "<label> No coupons homie </label>";
+    document.getElementById("couponSection").innerHTML = "<h3>Coupon Options</h3>\n\
+<label> You are not receiving any coupons. </label>";
 }
-function selectCoupons() {
-    document.getElementById("stuff").innerHTML = "<label> some coupons homie </label>";
+function selectCoupons(merchantList) {
+    var listOfMerchants = new Array();
+    listOfMerchants = merchantList.split("#");
+    var htmlString = "<h3>Advertisement Options</h3>\n\
+<label for='couponSortType'>Sort Coupons by:</label>\n\
+<select name=sortType id=sortType>\n\
+<option value='productType'>Product Type</option>\n\
+<option value='alphaNum'>Alphabetical</option>\n\
+<option value='distance'>Distance</option>\n\
+</select>\n\
+<br/>\n\
+<div id='merchantList'>";
+    
+    for (var i=0;i<listOfMerchants.length;i++)
+        {
+            if (listOfMerchants[i] != "" && listOfMerchants[i] != null)
+                {
+            htmlString += "<label for='" + listOfMerchants[i] + "'>" + listOfMerchants[i] + "</label>";
+            htmlString += "<input type='checkbox' name='" + listOfMerchants[i] + "' />";
+                }
+        }
+           htmlString += "</div>";
+
+   document.getElementById("couponSection").innerHTML = htmlString;
 }
 function allCoupons() {
-    document.getElementById("stuff").innerHTML = "<label> all coupons homie </label>";
+    document.getElementById("couponSection").innerHTML = "<h3>Coupon Options</h3>\n\
+<label> You are receiving coupons from the following merchants. </label>";
 }
