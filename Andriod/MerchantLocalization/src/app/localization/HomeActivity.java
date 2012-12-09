7e82657ff452f9cbed43ee275735d22520e68cd7
Daren Rodhouse <dkr32@nau.edu>
2012-12-09 15:15:27 -0700

    Merchant App initialization

---
+++ Andriod/MerchantLocalization/src/app/localization/HomeActivity.java
@@ -0,0 +1,40 @@
+package app.localization;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.location.*;
+
+
+public class HomeActivity extends Activity {
+	/** Called when the activity is first created. */
+
+
+	TextView username;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		Button logoutButton = (Button) findViewById(R.id.logoutButton);
+
+		//Listening to button event
+		logoutButton.setOnClickListener(new View.OnClickListener(){
+
+			public void onClick(View arg0) {
+				//Starting a new Intent
+				Intent homeScreen = new Intent(getApplicationContext(), LocalizationActivity.class);
+
+				startActivity(homeScreen);
+
+			}
+		});
+
+	}
+}
