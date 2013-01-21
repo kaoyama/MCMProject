3264f6799e9aeeb208ae10f536fb4b7d61da9679
Daren Rodhouse <dkr32@nau.edu>
2013-01-21 14:58:38 -0700

    Chihi stuff

---
+++ Andriod/Merchant/src/app/merchant/localiztion/MerchantLocalizationActivity.java
@@ -0,0 +1,40 @@
+package app.merchant.localiztion;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import app.merchantLocalization.R;
+
+public class MerchantLocalizationActivity extends Activity {
+    /** Called when the activity is first created. */
+	EditText inputUserName;
+    EditText inputPassword;
+    
+    TextView username;
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        inputUserName = (EditText) findViewById(R.id.username);
+        inputPassword = (EditText) findViewById(R.id.password);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+ 
+        //Listening to button event
+        loginButton.setOnClickListener(new View.OnClickListener(){
+ 
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent homeScreen = new Intent(getApplicationContext(), HomeActivity.class);
+ 
+                startActivity(homeScreen);
+ 
+            }
+        });
+    }
+}
