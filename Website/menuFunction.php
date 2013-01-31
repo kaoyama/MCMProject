2819f747a372afd66711c6d715b7bd5ff7304e8a
kaoyama <kd268@nau.edu>
2013-01-31 11:54:59 -0700

    Modified website

---
+++ Website/menuFunction.php
@@ -0,0 +1,15 @@
+<?php
+
+function displayMenu() {
+echo "<ul>
+            <li><a href='MCMhomePage.php'>Home</a></li>
+            <li><a href='MCMmerchantMap.php'>Merchant Map</a></li>
+            <li><a href='MCMcreateNewAccount.php'>Create New Account</a></li>
+            <li><a href='MCMaccountSettings.php'>Modify Account Settings</a></li>
+            <li><a href='MCMmerchantSettings.php'>Modify Merchant Settings</a></li>
+            <li><a href='MCMrecentActivities.php'>Recent Activities</a></li>
+            <li><a href='MCMcurrentLocation.php'>Input Current Location</a></li>
+            <li><a href='index.php'>Log out</a></li>
+        </ul>";
+}
+?>
