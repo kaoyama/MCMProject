6724f87b42462c177933cf3178837e7917f6e39d
kaoyama <kd268@nau.edu>
2013-03-05 15:02:28 -0700

    added script to add a transaction to the customerTransactions table

---
+++ Website/PHPAddUserTransaction.php
@@ -0,0 +1,38 @@
+<?php
+
+$databasehost = "acadgpl.ucc.nau.edu";
+$databasename = "kd268";
+$databaseusername ="kd268";
+$databasepassword = "capstone";
+
+// Connect to the database
+$con = mysql_connect($databasehost,$databaseusername,$databasepassword) 
+        or die(mysql_error());
+mysql_select_db($databasename) or die(mysql_error());
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+        
+// Get boolean value from customers table 
+
+$query = "INSERT INTO kd268.customerTransactions (merchant, customer, cost, paid)" . 
+        " VALUES ('" . $obj->{'merchant'} . "', '" . $obj->{'customer'} . "' , '"
+        . $obj->{'cost'} . "' , FALSE)";
+ 
+//echo $query;
+$res = mysql_query($query); 
+
+if (mysql_errno()) { 
+    header("HTTP/1.1 500 Internal Server Error");
+    echo $query.'<br>';
+    echo mysql_error(); 
+}
+else
+{
+    $rows = array();
+    while($r = mysql_fetch_assoc($res)) {
+        $rows[] = $r;
+    }
+    echo json_encode($rows);
+}
+?>
