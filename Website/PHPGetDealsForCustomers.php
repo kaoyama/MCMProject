db92d17dc5b3bb577fd593ffe49da1fda245c22a
kaoyama <kd268@nau.edu>
2013-03-31 17:48:18 -0700

    updated get deals for customer script

--- Website/PHPGetDealsForCustomers.php
+++ Website/PHPGetDealsForCustomers.php
@@ -9,9 +9,23 @@ $obj = json_decode($json);
 $userName = $obj->{'userName'};
 
 // subscribed merchants
-$query = "SELECT dealIndex, redeemed FROM kd268.customerDeals WHERE userName = '$userName'";
+$query = "SELECT dealIndex FROM kd268.customerDeals WHERE userName = '$userName' AND redeemed='0'";
 $result = $db->query($query); 
+$row = array();
 
-print $db->resultToJson($result); 
+// add deals from each subscribed merchants to the list
+while($r = mysql_fetch_assoc($result)) {
+    $dealIndex = $r['dealIndex'];
+
+    // query all ads/coupons from subscribed merchants 
+    $query = "SELECT title, content, merchant FROM kd268.deals 
+        WHERE dealIndex = '$dealIndex' AND enabled = TRUE AND approved = TRUE";    
+    $dealsRes = mysql_query($query); 
+    while($d = mysql_fetch_assoc($dealsRes)) {
+        $row[] = $d;
+    }
+}
+
+print $db->resultToJson($row); 
 
 ?>
